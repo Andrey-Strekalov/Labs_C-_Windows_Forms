76dd6727bec8620ca711b54e8a16160ccef87566
Andrey-Strekalov <andreistrekalov1994@gmail.com>
2025-10-15 14:30:34 +0300

    individual-task

--- WinFormsApp1/WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -28,27 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             textBox1 = new TextBox();
             fontDialog1 = new FontDialog();
+            bindingSource1 = new BindingSource(components);
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
             SuspendLayout();
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(12, 12);
+            textBox1.BackColor = SystemColors.Control;
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Segoe UI", 60F);
+            textBox1.Location = new Point(401, 89);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(150, 31);
+            textBox1.Size = new Size(340, 160);
             textBox1.TabIndex = 0;
+            textBox1.Text = "t = ";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(127, 154);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(150, 31);
+            textBox2.TabIndex = 1;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(127, 191);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(150, 31);
+            textBox3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15F);
+            label1.Location = new Point(48, 144);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 41);
+            label1.TabIndex = 3;
+            label1.Text = "g = ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15F);
+            label2.Location = new Point(48, 185);
+            label2.Name = "label2";
+            label2.Size = new Size(65, 41);
+            label2.TabIndex = 4;
+            label2.Text = "q =";
+            label2.Click += label2_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
             Controls.Add(textBox1);
             Name = "Form1";
             Text = "Form1";
             Click += Form1_Click;
             MouseMove += Form1_MouseMove;
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -57,5 +109,10 @@
 
         private TextBox textBox1;
         private FontDialog fontDialog1;
+        private BindingSource bindingSource1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
     }
 }
--- WinFormsApp1/WinFormsApp1/Form1.cs
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -2,6 +2,10 @@ namespace WinFormsApp1
 {
     public partial class Form1 : Form
     {
+        double t, a, z;
+        double g = 0;
+        double q = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -14,8 +18,27 @@ namespace WinFormsApp1
 
         private void Form1_MouseMove(object sender, MouseEventArgs e)
         {
-            Text = string.Format("Координаты: {0}, {1}", e.X, e.Y);
-            textBox1.Text = (e.X + e.Y).ToString();
+
+            a = (double)e.X;
+            z = (double)e.Y;
+            double.TryParse(textBox2.Text, out g);
+            double.TryParse(textBox3.Text, out q);
+
+            t = (a * z) / Math.Sqrt(Math.Pow(g, 3)) + (Math.Cos(g * q)) / (a * z) - g
+                + Math.Abs(Math.Sin(q) + Math.Sqrt(Math.Abs(12 - q)));
+
+            textBox1.Text = (t).ToString();
+        }
+
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
