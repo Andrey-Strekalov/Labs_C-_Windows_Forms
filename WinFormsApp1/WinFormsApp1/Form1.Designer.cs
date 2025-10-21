bfbe4459284aac774e20a1e6b2dd3ff7a6ed4ee2
Andrey-Strekalov <andreistrekalov1994@gmail.com>
2025-10-21 20:11:08 +0300

    study-task-1

--- WinFormsApp1/WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -28,12 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            menuStrip2 = new MenuStrip();
+            colorToolStripMenuItem = new ToolStripMenuItem();
+            toolStripComboBox1 = new ToolStripComboBox();
+            toolStripTextBox1 = new ToolStripTextBox();
+            toolStripTextBox2 = new ToolStripTextBox();
+            toolStripTextBox3 = new ToolStripTextBox();
+            toolStripSeparator1 = new ToolStripSeparator();
+            closeToolStripMenuItem = new ToolStripMenuItem();
+            button1 = new Button();
+            menuStrip2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip2
+            // 
+            menuStrip2.ImageScalingSize = new Size(24, 24);
+            menuStrip2.Items.AddRange(new ToolStripItem[] { colorToolStripMenuItem });
+            menuStrip2.Location = new Point(0, 0);
+            menuStrip2.Name = "menuStrip2";
+            menuStrip2.Size = new Size(800, 33);
+            menuStrip2.TabIndex = 1;
+            menuStrip2.Text = "menuStrip2";
+            // 
+            // colorToolStripMenuItem
+            // 
+            colorToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { toolStripComboBox1, toolStripTextBox1, toolStripTextBox2, toolStripTextBox3, toolStripSeparator1, closeToolStripMenuItem });
+            colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            colorToolStripMenuItem.Size = new Size(71, 29);
+            colorToolStripMenuItem.Text = "&Color";
+            // 
+            // toolStripComboBox1
+            // 
+            toolStripComboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            toolStripComboBox1.Items.AddRange(new object[] { "белый", "красный", "черный", "синий", "желтый" });
+            toolStripComboBox1.Name = "toolStripComboBox1";
+            toolStripComboBox1.Size = new Size(121, 33);
+            toolStripComboBox1.ToolTipText = "Готовые цвета";
+            toolStripComboBox1.SelectedIndexChanged += toolStripComboBox1_SelectedIndexChanged;
+            // 
+            // toolStripTextBox1
+            // 
+            toolStripTextBox1.Name = "toolStripTextBox1";
+            toolStripTextBox1.Size = new Size(100, 31);
+            toolStripTextBox1.Text = "0";
+            toolStripTextBox1.ToolTipText = "Красный";
+            toolStripTextBox1.TextChanged += toolStripTextBox1_TextChanged;
+            // 
+            // toolStripTextBox2
+            // 
+            toolStripTextBox2.Name = "toolStripTextBox2";
+            toolStripTextBox2.Size = new Size(100, 31);
+            toolStripTextBox2.Text = "0";
+            toolStripTextBox2.ToolTipText = "Зеленый";
+            toolStripTextBox2.TextChanged += toolStripTextBox1_TextChanged;
+            // 
+            // toolStripTextBox3
+            // 
+            toolStripTextBox3.Name = "toolStripTextBox3";
+            toolStripTextBox3.Size = new Size(100, 31);
+            toolStripTextBox3.Text = "0";
+            toolStripTextBox3.ToolTipText = "Синий";
+            toolStripTextBox3.TextChanged += toolStripTextBox1_TextChanged;
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(267, 6);
+            // 
+            // closeToolStripMenuItem
+            // 
+            closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            closeToolStripMenuItem.Size = new Size(270, 34);
+            closeToolStripMenuItem.Text = "E&xit";
+            closeToolStripMenuItem.Click += closeToolStripMenuItem_Click;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(31, 372);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 2;
+            button1.Text = "Close";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += closeToolStripMenuItem_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button1);
+            Controls.Add(menuStrip2);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            menuStrip2.ResumeLayout(false);
+            menuStrip2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private MenuStrip menuStrip2;
+        private ToolStripMenuItem colorToolStripMenuItem;
+        private ToolStripComboBox toolStripComboBox1;
+        private ToolStripTextBox toolStripTextBox1;
+        private ToolStripTextBox toolStripTextBox2;
+        private ToolStripTextBox toolStripTextBox3;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem closeToolStripMenuItem;
+        private Button button1;
     }
 }
