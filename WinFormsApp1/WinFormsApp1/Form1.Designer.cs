98dd5c748d898400e8087cb2d828bc7700cf8d94
Andrey-Strekalov <andreistrekalov1994@gmail.com>
2025-10-21 18:48:37 +0300

    individual-task

--- WinFormsApp1/WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -29,48 +29,52 @@
         private void InitializeComponent()
         {
             textBoxX = new TextBox();
-            textBoxY = new TextBox();
             textBoxN = new TextBox();
+            textBoxZ = new TextBox();
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
-            comboBoxW = new ComboBox();
+            comboBoxP = new ComboBox();
             label4 = new Label();
-            label5 = new Label();
             label6 = new Label();
             eq1 = new RadioButton();
             radioButton2 = new RadioButton();
-            textBoxZoo = new TextBox();
+            textBoxResult = new TextBox();
             label7 = new Label();
             button1 = new Button();
-            listBoxT = new ListBox();
+            comboBoxA = new ComboBox();
+            comboBoxB = new ComboBox();
+            label5 = new Label();
+            label8 = new Label();
+            listBoxR = new ListBox();
+            label9 = new Label();
             SuspendLayout();
             // 
             // textBoxX
             // 
-            textBoxX.Location = new Point(80, 66);
+            textBoxX.Location = new Point(41, 6);
             textBoxX.Name = "textBoxX";
             textBoxX.Size = new Size(150, 31);
             textBoxX.TabIndex = 0;
             // 
-            // textBoxY
-            // 
-            textBoxY.Location = new Point(80, 122);
-            textBoxY.Name = "textBoxY";
-            textBoxY.Size = new Size(150, 31);
-            textBoxY.TabIndex = 1;
-            // 
             // textBoxN
             // 
-            textBoxN.Location = new Point(80, 185);
+            textBoxN.Location = new Point(41, 49);
             textBoxN.Name = "textBoxN";
             textBoxN.Size = new Size(150, 31);
-            textBoxN.TabIndex = 2;
+            textBoxN.TabIndex = 1;
+            // 
+            // textBoxZ
+            // 
+            textBoxZ.Location = new Point(41, 92);
+            textBoxZ.Name = "textBoxZ";
+            textBoxZ.Size = new Size(150, 31);
+            textBoxZ.TabIndex = 2;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(35, 72);
+            label1.Location = new Point(12, 9);
             label1.Name = "label1";
             label1.Size = new Size(23, 25);
             label1.TabIndex = 3;
@@ -79,47 +83,38 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(35, 128);
+            label2.Location = new Point(12, 92);
             label2.Name = "label2";
             label2.Size = new Size(22, 25);
             label2.TabIndex = 4;
-            label2.Text = "Y";
+            label2.Text = "Z";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(35, 188);
+            label3.Location = new Point(10, 49);
             label3.Name = "label3";
             label3.Size = new Size(25, 25);
             label3.TabIndex = 5;
             label3.Text = "N";
             // 
-            // comboBoxW
+            // comboBoxP
             // 
-            comboBoxW.FormattingEnabled = true;
-            comboBoxW.Items.AddRange(new object[] { "0,05" });
-            comboBoxW.Location = new Point(353, 66);
-            comboBoxW.Name = "comboBoxW";
-            comboBoxW.Size = new Size(182, 33);
-            comboBoxW.TabIndex = 6;
+            comboBoxP.FormattingEnabled = true;
+            comboBoxP.Items.AddRange(new object[] { "0,1", "0,5", "1,0", "2,0", "5,0" });
+            comboBoxP.Location = new Point(308, 4);
+            comboBoxP.Name = "comboBoxP";
+            comboBoxP.Size = new Size(182, 33);
+            comboBoxP.TabIndex = 6;
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(307, 69);
+            label4.Location = new Point(269, 9);
             label4.Name = "label4";
-            label4.Size = new Size(29, 25);
+            label4.Size = new Size(22, 25);
             label4.TabIndex = 7;
-            label4.Text = "W";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Location = new Point(315, 122);
-            label5.Name = "label5";
-            label5.Size = new Size(21, 25);
-            label5.TabIndex = 8;
-            label5.Text = "T";
+            label4.Text = "P";
             // 
             // label6
             // 
@@ -132,10 +127,11 @@
             // 
             // eq1
             // 
+            eq1.Appearance = Appearance.Button;
             eq1.AutoSize = true;
             eq1.Location = new Point(598, 69);
             eq1.Name = "eq1";
-            eq1.Size = new Size(140, 29);
+            eq1.Size = new Size(125, 35);
             eq1.TabIndex = 11;
             eq1.TabStop = true;
             eq1.Text = "Уравнение 1";
@@ -152,25 +148,25 @@
             radioButton2.Text = "Уравнение 2";
             radioButton2.UseVisualStyleBackColor = true;
             // 
-            // textBoxZoo
+            // textBoxResult
             // 
-            textBoxZoo.Location = new Point(479, 356);
-            textBoxZoo.Name = "textBoxZoo";
-            textBoxZoo.Size = new Size(150, 31);
-            textBoxZoo.TabIndex = 13;
+            textBoxResult.Location = new Point(553, 326);
+            textBoxResult.Name = "textBoxResult";
+            textBoxResult.Size = new Size(150, 31);
+            textBoxResult.TabIndex = 13;
             // 
             // label7
             // 
             label7.AutoSize = true;
-            label7.Location = new Point(410, 357);
+            label7.Location = new Point(490, 332);
             label7.Name = "label7";
-            label7.Size = new Size(66, 25);
+            label7.Size = new Size(39, 25);
             label7.TabIndex = 14;
-            label7.Text = "Zoo = ";
+            label7.Text = "Z =";
             // 
             // button1
             // 
-            button1.Location = new Point(148, 359);
+            button1.Location = new Point(163, 368);
             button1.Name = "button1";
             button1.Size = new Size(112, 34);
             button1.TabIndex = 15;
@@ -178,36 +174,85 @@
             button1.UseVisualStyleBackColor = true;
             button1.Click += button1_Click;
             // 
-            // listBoxT
+            // comboBoxA
             // 
-            listBoxT.FormattingEnabled = true;
-            listBoxT.ItemHeight = 25;
-            listBoxT.Items.AddRange(new object[] { "0,001", "0,1", "10", "100", "1000", "10000" });
-            listBoxT.Location = new Point(353, 122);
-            listBoxT.Name = "listBoxT";
-            listBoxT.Size = new Size(182, 154);
-            listBoxT.TabIndex = 16;
+            comboBoxA.FormattingEnabled = true;
+            comboBoxA.Items.AddRange(new object[] { "0,1", "0,5", "1,0", "2,0", "5,0" });
+            comboBoxA.Location = new Point(310, 49);
+            comboBoxA.Name = "comboBoxA";
+            comboBoxA.Size = new Size(182, 33);
+            comboBoxA.TabIndex = 16;
+            // 
+            // comboBoxB
+            // 
+            comboBoxB.FormattingEnabled = true;
+            comboBoxB.Items.AddRange(new object[] { "0,1", "0,5", "1,0", "2,0", "5,0" });
+            comboBoxB.Location = new Point(308, 92);
+            comboBoxB.Name = "comboBoxB";
+            comboBoxB.Size = new Size(182, 33);
+            comboBoxB.TabIndex = 17;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(271, 52);
+            label5.Name = "label5";
+            label5.Size = new Size(24, 25);
+            label5.TabIndex = 18;
+            label5.Text = "A";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(271, 98);
+            label8.Name = "label8";
+            label8.Size = new Size(22, 25);
+            label8.TabIndex = 19;
+            label8.Text = "B";
+            // 
+            // listBoxR
+            // 
+            listBoxR.FormattingEnabled = true;
+            listBoxR.ItemHeight = 25;
+            listBoxR.Items.AddRange(new object[] { "5", "10", "15", "20", "25", "50" });
+            listBoxR.Location = new Point(41, 154);
+            listBoxR.Name = "listBoxR";
+            listBoxR.Size = new Size(180, 129);
+            listBoxR.TabIndex = 20;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(10, 206);
+            label9.Name = "label9";
+            label9.Size = new Size(23, 25);
+            label9.TabIndex = 21;
+            label9.Text = "R";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(listBoxT);
+            ClientSize = new Size(796, 463);
+            Controls.Add(label9);
+            Controls.Add(listBoxR);
+            Controls.Add(label8);
+            Controls.Add(label5);
+            Controls.Add(comboBoxB);
+            Controls.Add(comboBoxA);
             Controls.Add(button1);
             Controls.Add(label7);
-            Controls.Add(textBoxZoo);
+            Controls.Add(textBoxResult);
             Controls.Add(radioButton2);
             Controls.Add(eq1);
             Controls.Add(label6);
-            Controls.Add(label5);
             Controls.Add(label4);
-            Controls.Add(comboBoxW);
+            Controls.Add(comboBoxP);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
+            Controls.Add(textBoxZ);
             Controls.Add(textBoxN);
-            Controls.Add(textBoxY);
             Controls.Add(textBoxX);
             Name = "Form1";
             Text = "Form1";
@@ -218,20 +263,24 @@
         #endregion
 
         private TextBox textBoxX;
-        private TextBox textBoxY;
         private TextBox textBoxN;
+        private TextBox textBoxZ;
         private Label label1;
         private Label label2;
         private Label label3;
-        private ComboBox comboBoxW;
+        private ComboBox comboBoxP;
         private Label label4;
-        private Label label5;
         private Label label6;
         private RadioButton eq1;
         private RadioButton radioButton2;
-        private TextBox textBoxZoo;
+        private TextBox textBoxResult;
         private Label label7;
         private Button button1;
-        private ListBox listBoxT;
+        private ComboBox comboBoxA;
+        private ComboBox comboBoxB;
+        private Label label5;
+        private Label label8;
+        private ListBox listBoxR;
+        private Label label9;
     }
 }
--- WinFormsApp1/WinFormsApp1/Form1.cs
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -1,3 +1,7 @@
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
 namespace WinFormsApp1
 {
     public partial class Form1 : Form
@@ -5,35 +9,104 @@ namespace WinFormsApp1
         public Form1()
         {
             InitializeComponent();
-
-            comboBoxW.SelectedIndex = 0;
-            listBoxT.SelectedIndex = 0;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            double Zoo = 0;
-            double X = Convert.ToDouble(textBoxX.Text);
-            double Y = Convert.ToDouble(textBoxY.Text);
-            int N = Convert.ToInt32(textBoxN.Text);
-            double W = Convert.ToDouble(comboBoxW.SelectedItem.ToString());
-            double T = Convert.ToDouble(listBoxT.SelectedItem.ToString());
 
-            if (eq1.Checked)
+            try
             {
-                Zoo = 0;
-                double item = 0;
-                for (int i = 0; i < N; i++)
+                if (!double.TryParse(textBoxX.Text, out double x))
                 {
-                    item = (i % 2 == 0) ? Y : X;
-                    Zoo += Math.Pow(-1, i % 2) * Math.Pow(item, i);
+                    MessageBox.Show("Введите корректное значение X");
+                    return;
+                }
+
+                if (!int.TryParse(textBoxZ.Text, out int n) || n <= 0)
+                {
+                    MessageBox.Show("Введите корректное положительное значение N");
+                    return;
+                }
+
+                double Z = 0;
+
+                double X = Convert.ToDouble(textBoxX.Text);
+                int N = Convert.ToInt32(textBoxZ.Text);
+
+                if (eq1.Checked) // Первое уравнение 
+                {
+                    double p = Convert.ToDouble(comboBoxP.SelectedItem.ToString());
+                    Z = CalculateSeriesEquation(X, p, N);
+                }
+                else // Второе уравнение 
+                {
+                    double a = Convert.ToDouble(comboBoxA.SelectedItem.ToString());
+                    double b = Convert.ToDouble(comboBoxB.SelectedItem.ToString());
+                    int R = Convert.ToInt32(listBoxR.SelectedItem.ToString());
+                    Z = CalculateDoubleSumEquation(a, b, N, R);
+                }
+
+                textBoxResult.Text = Z.ToString("F6");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
+
+        // Метод для вычисления первого уравнения 
+        private double CalculateSeriesEquation(double X, double p, int N)
+        {
+            double result = 0;
+
+            for (int n = 1; n <= N; n++)
+            {
+                double term = CalculateSeriesTerm(X, p, n);
+                result += term;
+            }
+
+            return result;
+        }
+
+        // Вычисление n-го члена ряда
+        private double CalculateSeriesTerm(double X, double p, int n)
+        {
+          
+            double sign = (n % 2 == 1) ? -1 : 1; 
+            double pPower = (n == 1) ? 1 : Math.Pow(p, n - 1);
+            double xPower = Math.Pow(X, n);
+            double factorial = Factorial(n + 1);
+
+            return sign * pPower * xPower / factorial;
+        }
+
+        // Метод для вычисления второго уравнения 
+        private double CalculateDoubleSumEquation(double a, double b, int N, int R)
+        {
+            double result = 0;
+
+            for (int i = 1; i <= N; i++)
+            {
+                for (int j = 1; j <= R; j++)
+                {
+                    double numerator = Math.Pow(a * i + j, 2);
+                    double denominator = Math.Pow(i, 3) + b * Math.Pow(j, 3);
+                    result += numerator / denominator;
                 }
             }
-            else
+
+            return result;
+        }
+
+        // Вспомогательный метод для вычисления факториала
+        private double Factorial(int n)
+        {
+            double result = 1;
+            for (int i = 2; i <= n; i++)
             {
-                Zoo = W * X - T * Y + 7;
+                result *= i;
             }
-            textBoxZoo.Text = Zoo.ToString();
+            return result;
         }
     }
 }
