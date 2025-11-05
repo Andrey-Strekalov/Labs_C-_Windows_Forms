a0f05e65638aa1dd277e0f97c50ffc95c2e92adb
Andrey-Strekalov <andreistrekalov1994@gmail.com>
2025-11-05 11:53:18 +0300

    study-task

---
+++ WinFormsApp1/WinFormsApp1/DataInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    internal class DataInterface
+    {
+        extern List<RawDataItem> GetRawData();
+        extern List<SummaryDataItem> GetSummaryData();
+    }
+}
---
+++ WinFormsApp1/WinFormsApp1/RawDataItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1
+{
+    internal class RawDataItem
+    {
+        public String name { get; set; }
+        public int Group {  get; set; }
+        public String Part {  get; set; }
+        public float Price { get; set; }
+        public float Count { get; set; }
+        public float Summ
+        {
+            get { return Count * Price; }
+        }
+    }
+}
